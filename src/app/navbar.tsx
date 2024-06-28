9763ef8ac7b6993cc4200f20cf57679de03df394
= <phungnhatan2204@gmail.com>
2024-06-28 12:50:46 +0700

    Responsive for navbar, add MENU

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -7,6 +7,7 @@ import AuthService from "@/dbutils/userAPI/authservice";
 import { ButtonAsChild } from "@/components/ui/login-button";
 import UserProfile from "@/components/ui/user-profile-button";
 import { useRouter } from "next/navigation";
+import MenuButton from "@/components/ui/button-homepage-mobile";
 const NewNavbar = () => {
   const [isClient, setIsClient] = useState(false);
   const router = useRouter();
@@ -18,79 +19,10 @@ const NewNavbar = () => {
     <>
       {isClient && (
         <nav className="bg-white w-full flex relative justify-between items-center mx-auto px-7 h-20 z-50">
-          {/* Dropdown for small screens */}
-          <div className="block md:hidden">
-            <div className="relative inline-block text-left">
-              <span className="rounded-md shadow-sm">
-                <button
-                  className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
-                  type="button"
-                  aria-haspopup="true"
-                  aria-expanded="true"
-                  aria-controls="headlessui-menu-items-117"
-                >
-                  <span className="font-bold">MENU</span>
-                  <svg
-                    className="w-5 h-5 ml-2 -mr-1"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      values="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 111.414 1.414l-4 4a1 1 01-1.414 0l-4-4a1 1 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </span>
-              <div
-                className="hidden dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95"
-                id="headlessui-menu-items-117"
-              >
-                <div
-                  className="absolute left-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
-                  aria-labelledby="headlessui-menu-button-1"
-                  role="menu"
-                >
-                  <div className="px-4 py-3 font-semibold">
-                    <Link href="/">
-                      <p className="text-sm leading-5">HOME</p>
-                    </Link>
-                  </div>
-                  <div className="py-1">
-                    <Link
-                      href="/diamond"
-                      tabIndex={0}
-                      className="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left font-semibold"
-                      role="menuitem"
-                    >
-                      Diamond
-                    </Link>
-                    <Link
-                      href="/jewelry/page/1"
-                      tabIndex={1}
-                      className="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left font-semibold"
-                      role="menuitem"
-                    >
-                      Jewelry
-                    </Link>
-                    <Link
-                      href="/newrelease/page/1"
-                      tabIndex={2}
-                      className="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left font-semibold"
-                      role="menuitem"
-                    >
-                      New Release
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          
           {/* end dropdown */}
           {/* logo */}
-          <div className="inline-flex">
-            <div className="md:flex custom-hide">
+            <div className="hidden md:block">
               <Link
                 href="/diamond"
                 className="inline-block py-2 px-3 hover:bg-gray-200 rounded-full font-semibold"
@@ -116,21 +48,18 @@ const NewNavbar = () => {
                 </div>
               </Link>
             </div>
-          </div>
           {/* end logo */}
           {/* search bar */}
-          <div className="hidden sm:block flex-shrink flex-grow-0 justify-start px-2">
-            <div className="inline-block">
-              <div className="inline-flex items-center max-w-full">
+                <div className="visible md:invisible">
+                <MenuButton  />
+                </div>
                 <Link
                   href="/"
-                  className="flex items-center pb-4 border-b border-b-gray-400 md:block custom-hide"
+                  className="flex items-center py-3 border-b border-b-gray-400  text-sm md:text-2xl "
                 >
-                  <h2 className="font-bold text-2xl">H E P H A E S T U S</h2>
+                  <h2 className="font-bold ">H E P H A E S T U S</h2>
                 </Link>
-              </div>
-            </div>
-          </div>
+                
           {/* end search bar */}
           {/* login */}
           <div className="flex-initial">
@@ -157,6 +86,7 @@ const NewNavbar = () => {
               )}
             </div>
           </div>
+          
           {/* end login */}
         </nav>
       )}
