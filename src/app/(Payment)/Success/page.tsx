0b6a937a1582a1b51a84dabbe5e2e672c82549e8
PhatNLT <letienphat03@gmail.com>
2024-06-29 11:42:01 +0700

    Create payment success

---
+++ src/app/(Payment)/Success/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "@/app/(Payment)/Success/PaymentSuccess.css";
+import { Link } from "react-router-dom";
+
+const PaymentSuccess: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white shadow-lg rounded-lg p-6 max-w-sm w-full text-center">
+        <div className="flex justify-center mb-4">
+          <div className="success-checkmark">
+            <div className="check-icon">
+              <span className="icon-line line-tip"></span>
+              <span className="icon-line line-long"></span>
+              <div className="icon-circle"></div>
+              <div className="icon-fix"></div>
+            </div>
+          </div>
+        </div>
+        <div className="text-2xl font-bold text-green-600 mb-4">Payment Success!</div>
+        <div className="text-xl text-black mb-8">Thank you for your purchase!</div>
+        <div className="items-center">
+          <Link to="/" className="bg-black text-white py-4 px-6 rounded hover:bg-gray-900 focus:outline-none">
+            Go Back to Store
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PaymentSuccess;
